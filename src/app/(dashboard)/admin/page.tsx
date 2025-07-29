2ca2e45541156d03f4cf5a6aeb803e8f0882fc17
Paulo Borges <dev.pauloborges@gmail.com>
2025-07-28 21:48:09 -0300

    feat: proper data fetching for admin charts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,15 @@
 import Announcements from "@/components/Announcements"
-import AttendanceChart from "@/components/AttendanceChart"
+import AttendanceChartContainer from "@/components/AttendanceChartContainer"
 import CountChartContainer from "@/components/CountChartContainer"
-import EventCalendar from "@/components/EventCalendar"
+import EventCalendarContainer from "@/components/EventCalendarContainer"
 import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
-const AdminPage = () => {
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
@@ -27,7 +31,7 @@ const AdminPage = () => {
 
           {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart />
+            <AttendanceChartContainer />
           </div>
         </div>
 
@@ -39,7 +43,7 @@ const AdminPage = () => {
 
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
